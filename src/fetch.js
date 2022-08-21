78eb4c85ae40873168842f3f91e59fdc5d0e18d1
Lucio Ivan Benito <lucio_21@live.com.mx>
2022-08-21 02:08:02 -0500

    add fetch for practice

--- src/fetch.js
+++ src/fetch.js
@@ -1,3 +1,5 @@
+//import Compressor from "../../node_modules/compressorjs/dist/compressor.esm.js";
+/* ######### SE REALIZA PETICION FETCH SOLO PARA PRACTICAR PERO NO ES RECOMENDABLE OBTENER IMG VIA API ES MEJOR DE MANERA LOCAL ####### */
 const API_KEY = "MWzFiiifrN5rFbNftQKoJDZlKkVW8j5Oyo0SzAKwvV0";
 const params = {
   method: 'GET',
@@ -7,30 +9,27 @@ const params = {
   //"key": API_KEY,
   //"query": "canada",
 }
-const randomNumber = Math.floor(Math.random()*10)
+const randomNumber = Math.floor(Math.random() * 10)
 const img = document.querySelector('#img-api')
 const loading = document.querySelector('#loading')
 
 async function fetchImg() {
   const response = await fetch(`https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=gamer`, params)
   const data = await response.json()
-  //console.log(data.results[randomNumber].urls.raw)
+  //console.log(data)
   const url = data.results[randomNumber].urls.raw;
   return url
 }
 fetchImg()
-  .then( urlImg => {
-    
+  .then(urlImg => {
+    // const imgCompress = new Compressor(urlImg)
     img.src = urlImg;
     img.onload = function () {
-      img.style.transform = 'scale(1)';
       loading.style.display = 'none';
       img.style.display = 'block';
-     
-      console.log('cargada la img')
+
+      //console.log('cargada la img')
     }
-    
-    
-})
 
 
+  })
